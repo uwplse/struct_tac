7682101f8ef211344cd344bf6f5a11a2cbbcc44b
Jared Roesch <roeschinc@gmail.com>
2017-11-02 17:20:03 -0700

    Restore simp_coe

--- src/struct_tact/default.lean
+++ src/struct_tact/default.lean
@@ -7,6 +7,10 @@ import .break_tactics
 open tactic
 open interactive
 
+meta def simp_coe :=
+   `[unfold coe lift_t has_lift_t.lift coe_t has_coe_t.coe coe_b has_coe.coe,
+     try { dsimp * at * }]
+
 -- (* https://github.com/uwplse/StructTact *)
 
 -- Ltac subst_max :=
