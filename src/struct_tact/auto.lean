7da6deb37074346501881050e7a9a62c77a16460
Jared Roesch <roeschinc@gmail.com>
2017-11-26 01:05:26 -0800

    Add better auto implementation

--- src/struct_tact/auto.lean
+++ src/struct_tact/auto.lean
@@ -3,12 +3,16 @@ open lean
 open interactive.types
 open tactic
 
+@[user_attribute] meta def auto_lemma_attr : user_attribute unit :=
+{ name := `auto_lemma, descr := "Mark a lemma availble for use in the auto tactic." }
+
 meta def auto_attr_handler (inductive_name : name) : command :=
 do e ← get_env,
    when (¬e.is_inductive inductive_name)
-     (fail "constructor tactic failed, target is not an inductive datatype"),
+     (fail $ "auto attribute failed, target is not an inductive datatype: " ++ to_string inductive_name),
    let ctors := e.constructors_of inductive_name,
-   monad.mapm (fun ctor, set_basic_attribute `intro ctor) ctors,
+   monad.mapm (fun ctor,
+    do user_attribute.set auto_lemma_attr ctor () tt) ctors,
    return ()
 
 @[user_attribute] meta def auto_attr : user_attribute unit :=
@@ -16,4 +20,9 @@ do e ← get_env,
   after_set := some (λ n _ _, auto_attr_handler n)
 }
 
-meta def auto := tactic.back_chaining_using_hs
+meta def auto : tactic unit :=
+do ls ← local_context,
+   ns ← attribute.get_instances `auto_lemma,
+   cs ← monad.mapm (fun n, mk_const n) ns,
+   -- trace $ ("Lemmas: " ++ to_string ls ++ to_string cs),
+   tactic.back_chaining_using (ls ++ cs) <|> tactic.fail "auto tactic failed"
--- test/auto_test.lean
+++ test/auto_test.lean
@@ -1,6 +1,11 @@
--- import ..src.struct_tact
+import ..src.struct_tact
 
--- @[auto] inductive rel : nat → Type
--- | a : rel 0
--- | b : rel 1
--- | c : rel 2
+@[auto] inductive rel : nat → Type
+| a : rel 0
+| b : rel 1
+| c : rel 2
+
+lemma test_1 : rel 2 :=
+begin
+    auto,
+end
